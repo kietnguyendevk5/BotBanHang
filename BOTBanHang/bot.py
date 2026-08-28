7c494681ece54b554b2d7b270988e7180d4c5ee8
kietnguyendevk5 <galu17450@gmail.com>
2026-08-29 03:41:36 +0700

    Update bot.py

--- BOTBanHang/bot.py
+++ BOTBanHang/bot.py
@@ -123,7 +123,9 @@ def get_category_info_by_filename(filename):
     elif "BM" in fname:
         return ("cat_bm", "Clone New đã qua BM", 2500, "Hàng login qua cookies, ae log id pass tets trước khi dùng")
     elif "TRUST" in fname or "2FA" in fname:
-        return ("cat_fb_2fa_trust", "CLONE NGÂM TRÂU - NAME RANDOM - ON2FA, NO AVT ", 3000, "UID | Pass | 2FA |COOKIE|TOKEN EAAAAU| Hotmail | Pass Hotmail")    
+        return ("cat_fb_2fa_trust", "CLONE NGÂM TRÂU - NAME RANDOM - ON2FA, NO AVT ", 3000, "UID | Pass | 2FA |COOKIE|TOKEN EAAAAU| Hotmail | Pass Hotmail") 
+    elif "VIA" in fname or "CO" in fname:
+        return ("cat_via_co", "CLONE ĐẦU 1000x - NAME NGOẠI - ON2FA, AVT ", 150000, "UID | Pass | 2FA |COOKIE|TOKEN EAAAAU| Hotmail | Pass Hotmail|MKP")    
     else:
         return ("cat_new_zin", "CLONE NGÂM TRÂU - NAME RANDOM - VER HOTMAIL - LIVE ALL 100%", 2000, "UID | PASS | HOTMAIL| COOKIE|TOKEN EAAAAU")
 
