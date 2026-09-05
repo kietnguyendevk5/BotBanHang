4508745c70b66736de31266e08208a12062bf054
kietnguyendevk5 <galu17450@gmail.com>
2026-09-05 10:25:04 +0700

    Update bot.py

--- BOTBanHang/bot.py
+++ BOTBanHang/bot.py
@@ -150,6 +150,8 @@ def get_category_info_by_filename(filename):
         return ("cat_gmail", "CLONE NGÂM TRÂU - NAME RANDOM - ON2FA - NO AVT - VER GMAIL ", 3500, "UID | Pass | 2FA |COOKIE|TOKEN EAAAAU|MAIL")
     elif "PHONE" in fname:
         return ("cat_phone", "CLONE NEW - NAME VIỆT - ON2FA - AVT - 3 POST - VER PHONE ", 2500, "UID | Pass | 2FA |COOKIE|TOKEN EAAAAU")
+    elif "DIEMAIL" in fname:
+        return ("cat_die_mail", "CLONE NGÂM - NAME VIỆT - ON2FA - AVT ADD 20 -50 BB - VER HOTMAIL - MAIL DIE ", 3500, "UID | Pass | 2FA |COOKIE|TOKEN EAAAAU(MAIL DIE)")    
     else:
         return ("cat_new_zin", "CLONE NGÂM TRÂU - NAME RANDOM - VER HOTMAIL - LIVE ALL 100%", 2000, "UID | PASS | HOTMAIL| COOKIE|TOKEN EAAAAU")
 
